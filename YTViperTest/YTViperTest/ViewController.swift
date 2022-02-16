977fe3003ad4a89104571ac8ec3c437f26e8819f
yohei_takasaki <y.takasaki@speakbuddy.jp>
2022-02-16 23:54:06 +0900

    初回up

---
+++ YTViperTest/YTViperTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YTViperTest
+//
+//  Created by ようへい on 2022/02/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
